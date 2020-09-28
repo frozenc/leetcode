5374a33548b02704108878c6ca1d768082441d5a
frozenchan <jiebinchan@gmail.com>
2020-09-28 13:59:59 +0800

    2020-09-28

--- src/leetcode/Tree/Medium_117.java
+++ src/leetcode/Tree/Medium_117.java
@@ -45,4 +45,41 @@ public class Medium_117 {
         }
         return root;
     }
+
+    //O(1)空间复杂度
+    private Node last;
+    private Node nextStart;
+
+    public Node connect2(Node root) {
+        if (root == null) {
+            return root;
+        }
+
+        Node start = root;
+        while (start != null) {
+            last = null;
+            nextStart = null;
+            for (Node p = start; p != null; p = p.next) {
+                if (p.left != null) {
+                    handle(p.left);
+                }
+                if (p.right != null) {
+                    handle(p.right);
+                }
+            }
+            start = nextStart;
+        }
+
+        return root;
+    }
+
+    public void handle(Node p) {
+        if (last != null) {
+            last.next = p;
+        }
+        if (nextStart == null) {
+            nextStart = p;
+        }
+        last = p;
+    }
 }
