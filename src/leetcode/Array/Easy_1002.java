16574f683a8a081981a0737cf9cb12bc328b9597
frozenchan <jiebinchan@gmail.com>
2020-10-14 14:34:36 +0800

    2020-10-14

--- src/leetcode/Array/Easy_1002.java
+++ src/leetcode/Array/Easy_1002.java
@@ -1,9 +1,6 @@
 package leetcode.Array;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * leetcode
@@ -36,6 +33,30 @@ public class Easy_1002 {
             int num = map.get(c);
             for (int i = 0; i < num; i++) {
                 res.add(String.valueOf(c));
+            }
+        }
+        return res;
+    }
+
+    public List<String> commonChars2(String[] A) {
+        List<String> res = new ArrayList<>();
+        if (A.length == 0) {
+            return res;
+        }
+        int[] minFreq = new int [26];
+        Arrays.fill(minFreq, Integer.MAX_VALUE);
+        for (String str : A) {
+            int[] freq = new int[26];
+            int len = str.length();
+            for (int i = 0; i < len; i++) {
+                int idx = str.charAt(i) - 'a';
+                freq[idx] = Math.min(freq[idx] + 1, minFreq[idx]);
+            }
+            minFreq = freq;
+        }
+        for (int i = 0; i < minFreq.length; i++) {
+            for (int j = 0; j < minFreq[i]; j++) {
+                res.add(String.valueOf((char)('a' + i)));
             }
         }
         return res;
